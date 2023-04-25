e73e8d41814847453f98a023f9a4ce282859ebbb
Spatzy <55000624+1spatz1@users.noreply.github.com>
2023-04-25 14:27:58 +0200

    Added Customer and start of Order model

--- Bernard-Mulder/src/Database/DatabaseQueries.java
+++ Bernard-Mulder/src/Database/DatabaseQueries.java
@@ -7,22 +7,9 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class DatabaseQueries {
-    // !_ note _! this is just init
-    // it will not create a connection
+    // Init for database
     DatabaseConnector databaseConnector = new DatabaseConnector();
     private ArrayList<Product> products;
-    //        String sql = "SELECT * FROM `orders`";
-//        try {
-//            PreparedStatement statement = database.connect().prepareStatement(sql);
-//            ResultSet resultSet = statement.executeQuery();
-//            while(resultSet.next()){
-//                System.out.println("OrderID: " + resultSet.getInt("OrderID") + " CustomerId: " + resultSet.getInt("CustomerID"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            database.disconnect();
-//        }
     public ArrayList<Product> getProducts(){
         products = new ArrayList<>();
         String sql = "SELECT * FROM `stockitems` LEFT JOIN `stockitemholdings` ON stockitems.StockItemID = stockitemholdings.StockItemID";
