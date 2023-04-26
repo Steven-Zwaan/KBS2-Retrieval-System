a4eb2528f7574475bc42706a443c32decf25171c
Thimo812 <114487448+Thimo812@users.noreply.github.com>
2023-04-26 12:53:49 +0200

    bezig met arduino connectie

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -2,7 +2,7 @@
 
 #define VRX_PIN  A2 // Arduino pin connected to VRX pin
 #define VRY_PIN  A3 // Arduino pin connected to VRY pin
-#define SW_PIN   A0  // Arduino pin connected to SW  pin
+#define SW_PIN   7  // Arduino pin connected to SW  pin
 
 
 #define LEFT_THRESHOLD  200
@@ -19,13 +19,15 @@
 ezButton button(SW_PIN);
 
 // Variabelen
-
+const int zPin = A5;
 int XPWM = 11;
 int XDir = 13;
 int YDir = 12;
 int YPWM= 3;
 bool Links = LOW;
 bool Rechts = HIGH;
+
+bool zAs = false;
 
 
 int xValue = 0; // To store value of the X axis
@@ -44,8 +46,7 @@ pinMode (YDir, OUTPUT);
 pinMode (YPWM, OUTPUT);
 pinMode(VRX_PIN, INPUT);
 pinMode(VRY_PIN, INPUT);
-pinMode(SW_PIN, INPUT);
-digitalWrite(SW_PIN, HIGH);
+pinMode(zPin, OUTPUT);
 
 }
 
@@ -60,85 +61,72 @@ void loop() {
  // converts the analog value to commands
   // reset commands
   command = COMMAND_NO;
-
+  if (!zAs) {
   // check left/right commands
-  if (xValue < LEFT_THRESHOLD)
-    command = command | COMMAND_LEFT;
-  else if (xValue > RIGHT_THRESHOLD)
-    command = command | COMMAND_RIGHT;
+    if (xValue < LEFT_THRESHOLD)
+      command = command | COMMAND_LEFT;
+    else if (xValue > RIGHT_THRESHOLD)
+      command = command | COMMAND_RIGHT;
 
-  // check up/down commands
-  if (yValue < UP_THRESHOLD)
-    command = command | COMMAND_UP;
-  else if (yValue > DOWN_THRESHOLD)
-    command = command | COMMAND_DOWN;
+    // check up/down commands
+    if (yValue < UP_THRESHOLD)
+      command = command | COMMAND_UP;
+    else if (yValue > DOWN_THRESHOLD)
+      command = command | COMMAND_DOWN;
 
-  // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
+    // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
 
-  // print command to serial and process command
-  if (command & COMMAND_LEFT) {
-    Serial.println("COMMAND LEFT");
-    int motorSpeed = 255 - map(xValue, 0, 1023, 0, 255);
-    digitalWrite(XDir, LOW);
-    analogWrite(XPWM, motorSpeed);
-    Serial.println(motorSpeed);
+    // print command to serial and process command
+    if (command & COMMAND_LEFT) {
+      int motorSpeed = 255 - map(xValue, 0, 1023, 0, 255);
+      digitalWrite(XDir, LOW);
+      analogWrite(XPWM, motorSpeed);
 
-     //TODO: add your task here
-  }  else if (command & COMMAND_RIGHT) {
-    Serial.println("COMMAND RIGHT");
+      //TODO: add your task here
+    }  else if (command & COMMAND_RIGHT) {
+      
+      int motorSpeed = map(xValue, 512, 1023, 0, 255);
+      digitalWrite(XDir, HIGH);
+      analogWrite(XPWM, motorSpeed);
+    } else  {
+      analogWrite(XPWM, 0);
+      command;
+
+    }
+  
+    if (command & COMMAND_UP) {
+      // TODO: add your task here
+      int motorSpeed = 255 - map(yValue, 0, 512, 0, 255);
+      digitalWrite(YDir, LOW);
+      analogWrite(YPWM, motorSpeed);
+
+    } else if (command & COMMAND_DOWN) {
+      // TODO: add your task here
+      int motorSpeed = map(yValue, 513, 1023, 0, 255);
+      digitalWrite(YDir, HIGH);
+      analogWrite(YPWM, motorSpeed);
+
+    } else  {
+      analogWrite(YPWM, 0);
+      command;
+    }
+  } else {
+    Serial.println(yValue);
+    analogWrite(zPin, yValue);
     
-    int motorSpeed = map(xValue, 512, 1023, 0, 255);
-    digitalWrite(XDir, HIGH);
-    analogWrite(XPWM, motorSpeed);
-    Serial.println(motorSpeed);
-  } else  {
-    analogWrite(XPWM, 0);
-    command;
-
-  }
- 
-  if (command & COMMAND_UP) {
-    Serial.println("COMMAND UP");
-    // TODO: add your task here
-    int motorSpeed = 255 - map(yValue, 0, 1023, 0, 255);
-    digitalWrite(YDir, LOW);
-    analogWrite(YPWM, motorSpeed);
-    Serial.println(motorSpeed);
-
-  } else if (command & COMMAND_DOWN) {
-    Serial.println("COMMAND DOWN");
-    // TODO: add your task here
-    int motorSpeed = map(yValue, 512, 1023, 0, 255);
-    digitalWrite(YDir, HIGH);
-    analogWrite(YPWM, motorSpeed);
-    Serial.println(motorSpeed);
-
-  } else  {
-    analogWrite(YPWM, 0);
-    command;
   }
 
-// Read the button value
   bValue = button.getState();
 
- if (button.isPressed()) {
-    Serial.println("The button is pressed");
-    // TODO do something here
-  }
 
-  if (button.isReleased()) {
-    Serial.println("The button is released");
-    // TODO do something here
+ if (button.isPressed() && zAs == false) {
+    zAs = true;
+  } else if (button.isPressed()) {
+    zAs = false;
   }
 
  //analogWrite(XPWM, 0);
  //digitalWrite(XDir, LOW);
-
-  Serial.print("x = ");
-  Serial.print(analogRead(VRX_PIN));
-  Serial.print(", y = ");
-  Serial.println(analogRead(VRY_PIN));
-  delay(200);
   
 
 }
