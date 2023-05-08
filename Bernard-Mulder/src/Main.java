dbd4605d85ca8585c7a145c58df0f435abba886b
Flipsing <nova.starke123@gmail.com>
2023-05-08 13:09:01 +0200

    commit

--- Bernard-Mulder/src/Main.java
+++ Bernard-Mulder/src/Main.java
@@ -2,29 +2,29 @@ import Entities.*;
 
 public class Main {
     public static void main(String[] args) {
-        //MainScreen scherm = new MainScreen();
-        Noodstop schermNoodstop = new Noodstop();
+        MainScreen scherm = new MainScreen();
+        //Noodstop schermNoodstop = new Noodstop();
         ArduinoConnection arduino = new ArduinoConnection("COM3");
 
-        Products products = new Products();
-        products.storeProductsFromDatabase();
-        products.storeProductFromDatabase(98);
-        for (Product p: products.getProducts()){
-            p.setStock(30);
-        }
-        products.storeProductFromDatabase(198);
-        for (Product p: products.getProducts()){
-            System.out.println(p.toString());
-        }
-        products.storeProductFromDatabase(98);
-        for (Product p: products.getProducts()){
-            System.out.println(p.toString());
-            p.setStock(25);
-        }
-        products.storeProductFromDatabase(98);
-        for (Product p: products.getProducts()){
-            System.out.println(p.toString());
-        }
+//        Products products = new Products();
+//        products.storeProductsFromDatabase();
+//        products.storeProductFromDatabase(98);
+//        for (Product p: products.getProducts()){
+//            p.setStock(30);
+//        }
+//        products.storeProductFromDatabase(198);
+//        for (Product p: products.getProducts()){
+//            System.out.println(p.toString());
+//        }
+//        products.storeProductFromDatabase(98);
+//        for (Product p: products.getProducts()){
+//            System.out.println(p.toString());
+//            p.setStock(25);
+//        }
+//        products.storeProductFromDatabase(98);
+//        for (Product p: products.getProducts()){
+//            System.out.println(p.toString());
+//        }
 
         Orders orders = new Orders();
         orders.storeOrdersFromDatabase();
