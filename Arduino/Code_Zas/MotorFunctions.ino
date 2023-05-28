43874b7076baf805023b310144877d4ae36a6bd1
Spatzy <55000624+1spatz1@users.noreply.github.com>
2023-05-28 13:19:34 +0200

    comments z-as grijp actie toegevoegd

--- Arduino/Code_Zas/MotorFunctions.ino
+++ Arduino/Code_Zas/MotorFunctions.ino
@@ -66,31 +66,31 @@ bool motorYgoTo(int yPosition){
 }
 
 bool motorZpickUp(int zPosition){
-  while(!packagePicked){
-    while(!inPosition){
-      if (readIR() < zPosition){
-        motorZforward();
-      } else if (readIR() > zPosition){
-       motorZbackward();
-      } else {
-       motorZstop();
-       yPositioned = false;
-       zReturned = false;
-       inPosition = true;
+  while(!packagePicked){ // kijk of het product nog niet gegrepen is
+    while(!inPosition){ // loop terwijl de z-as nog niet in positie is
+      if (readIR() < zPosition){ // check of de z-as positie kleiner is dan eind positie
+        motorZforward(); // laat z-as vooruit gaan
+      } else if (readIR() > zPosition){ // check of de z-as positie groter is dan eind positie
+       motorZbackward(); // laat z-as achteruit gaan
+      } else { // else als de z-as in positie is
+       motorZstop(); // stop z-as motor
+       yPositioned = false; // zet y positie op false
+       zReturned = false; // zet z-as returned op false
+       inPosition = true; // zet dat z-as in positie is
       }
     }
-    if(motorYgoTo(yPosBoxes[hmi_y] + 100)){
-      yPositioned = true;
+    if(motorYgoTo(yPosBoxes[hmi_y] + 100)){ // laat motor naar huidige y-as positie gaan + 100 omhoog
+      yPositioned = true; // zet y positie op true
     }
-    if(yPositioned){
-      while(!zReturned){
-      motorZbackward();
-        if(readIR() == 5){
-          motorZstop();
-          zReturned = true;
-          packagePicked = true;
-          doneZ = true;
-          return 1;
+    if(yPositioned){ // controleer of y-as in positie is
+      while(!zReturned){ / loop als z-as niet op start positie is
+      motorZbackward(); // laat z-as achteruit gaan
+        if(readIR() == 5){ // actie als z-as op 5(cm) is oftewel op start positie
+          motorZstop(); // stop z-as motor
+          zReturned = true; // zet z-as returned op true
+          packagePicked = true; // zet product gegrepen op true
+          doneZ = true; // zet done actie z-as op true
+          return 1; // return 1 dat de functie uitgevoerd is
         }
       }
     }
