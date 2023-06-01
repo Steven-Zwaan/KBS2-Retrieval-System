98a7a7d5f0f84f1b0159f01e2da3fc358e7940d2
Steven-Zwaan <s1188117@student.windesheim.nl>
2023-06-01 13:22:55 +0200

    Queue

--- Bernard-Mulder/src/main/java/Main.java
+++ Bernard-Mulder/src/main/java/Main.java
@@ -6,17 +6,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         MainScreen scherm = new MainScreen();
-        ArduinoConnection arduino = new ArduinoConnection("/dev/ttyACM0");
-//
-        arduino.openPort();
-        arduino.receiveData();
-        Thread.sleep(2000);
-//
-//
-        arduino.sendData((byte) 3, (byte) 1, (byte) 0);
-        arduino.sendData((byte) 3, (byte) 3, (byte) 2);
-//        arduino.sendData((byte) 2, (byte) 0, (byte) 0);
-//        arduino.sendData((byte) 1, (byte) 2, (byte) 1);
+
+        // ArduinoConnection arduino = new ArduinoConnection("COM8");
+       Queue instance = Queue.getInstance();
+
+        // arduino.openPort();
+        // arduino.receiveData();
+
+        // arduino.sendData((byte) 3, (byte) 1, (byte) 0);
+        // arduino.sendData((byte) 3, (byte) 3, (byte) 2);
+
 
 
 
---
+++ Bernard-Mulder/src/main/java/Queue.java
@@ -0,0 +1,75 @@
+import Route.Point;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Queue {
+	ArrayList<ArrayList<Route.Point>> queue = new ArrayList<>();
+	boolean active = false;
+	boolean messageNotSend = true;
+	public static Queue instance = null;
+
+	public static Queue getInstance() {
+		if(instance == null) {
+			instance = new Queue();
+		}
+		return instance;
+	}
+
+	private Queue() {
+
+		ArduinoConnection arduino = new ArduinoConnection("COM8");
+
+		arduino.openPort();
+		arduino.receiveData();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					if (active){
+						if (messageNotSend){
+							for (int i = 0; i < queue.get(0).size(); i++) {
+								try {
+									arduino.sendData((byte) 3, (byte) queue.get(0).get(i).getX(), (byte) queue.get(0).get(i).getY());
+								} catch (IOException e) {
+									throw new RuntimeException(e);
+								} catch (InterruptedException e) {
+									throw new RuntimeException(e);
+								}
+								System.out.println(queue.get(0).get(i));
+							}
+							queue.remove(0);
+							messageNotSend = false;
+						}
+					}
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+					checkQueue();
+				}
+			}
+		}).start();
+	}
+
+	public void AddQueue(ArrayList<Point> PickupRoute) {
+		queue.add(PickupRoute);
+//		System.out.println(queue.get(0));
+	}
+
+	public void checkQueue(){
+		if (queue.size() == 0) {
+			active = false;
+		} else {
+			active = true;
+		}
+	}
+}
